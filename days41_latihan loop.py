18a2e3338e167f658d597b366c45ccc915378304
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-21 08:40:24 +0800

    Add files via upload

---
+++ days41_latihan loop.py
@@ -0,0 +1,8 @@
+baris = int(input("masukkan nilai baris:"))
+kolom = int(input("masukkan nilai kolom:"))
+
+for i in range (baris):
+    for i in range (kolom):
+        print ("0", end = " ")
+    else:
+        print(" ")
